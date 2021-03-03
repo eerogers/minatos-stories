75567bb82942eb848370a43c91205ab93981b8ca
Elizabeth Rogers <elle.eels@gmail.com>
2021-03-02 23:01:50 -0600

    soundboard and connectivity pages

--- code.js
+++ code.js
@@ -3,35 +3,39 @@ $(document).on("click", "#nextPage", function(){
 })
 
 $(document).on("click", "#once", function(){
-    playAudio("Once.mp3")
+    playAudio("TheHauntedTreasure/Once.mp3")
 })
 
 $(document).on("click", "#upon", function(){
-    playAudio("Upon.mp3")
+    playAudio("TheHauntedTreasure/Upon.mp3")
 })
 
 $(document).on("click", "#a", function(){
-    playAudio("A.mp3")
+    playAudio("TheHauntedTreasure/A.mp3")
 })
 
 $(document).on("click", "#time", function(){
-    playAudio("Time.mp3")
+    playAudio("TheHauntedTreasure/Time.mp3")
 })
 
 $(document).on("click", "#there", function(){
-    playAudio("There.mp3")
+    playAudio("TheHauntedTreasure/There.mp3")
 })
+
 $(document).on("click", "#was", function(){
-    playAudio("Was.mp3")
+    playAudio("TheHauntedTreasure/Was.mp3")
 })
+
 $(document).on("click", "#boy", function(){
-    playAudio("Boy.mp3")
+    playAudio("TheHauntedTreasure/Boy.mp3")
 })
+
 $(document).on("click", "#named", function(){
-    playAudio("Named.mp3")
+    playAudio("TheHauntedTreasure/Named.mp3")
 })
+
 $(document).on("click", "#Minato", function(){
-    playAudio("Minato.mp3")
+    playAudio("TheHauntedTreasure/Minato.mp3")
 })
 function playAudio(url) {
     new Audio(url).play();
